4fed4c30b80f782aab69b755c5c6fcdaf87e1c9c
hana-piyajong <hana.piyajong@gmail.com>
2025-06-02 20:37:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,9 @@ model = load_model()
 # Transform image
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
 ])
 
 # Streamlit UI
